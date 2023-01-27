8d880c5da6ecef229e985ec216f33e31cc7d07bf
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-01-27 17:37:47 -0500

    Initial commit

---
+++ src/main/java/net/sirjain/convenient_hitboxes/ConvenientHitboxesMain.java
@@ -0,0 +1,33 @@
+package net.sirjain.convenient_hitboxes;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ConvenientHitboxesMain.MOD_ID)
+public class ConvenientHitboxesMain {
+    public static final String MOD_ID = "convenient_hitboxes";
+
+    public ConvenientHitboxesMain() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        System.out.println("Initializing Convenient Hitboxes for Forge!");
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
